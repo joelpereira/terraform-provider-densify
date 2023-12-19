1b389d07a5c4f20a87c0e47c067a786bc6271687
Joel Pereira <joelpereira@users.noreply.github.com>
2023-12-19 12:06:49 -0500

    Updated comments for godot

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 	"terraform-provider-densify/internal/provider"
 )
 
-// Run "go generate" to format example terraform files and generate the docs for the registry/website
+// Run "go generate" to format example terraform files and generate the docs for the registry/website.
 
 // If you do not have terraform installed, you can remove the formatting command, but its suggested to
 // ensure the documentation is formatted properly.
@@ -24,11 +24,10 @@ import (
 //go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
 
 var (
-	// these will be set by the goreleaser configuration
-	// to appropriate values for the compiled binary.
+	// these will be set by the goreleaser configuration to appropriate values for the compiled binary.
 	version string = "dev"
 
-	// goreleaser can pass other information to the main package, such as the specific commit
+	// goreleaser can pass other information to the main package, such as the specific commit.
 	// https://goreleaser.com/cookbooks/using-main.version/
 )
 
